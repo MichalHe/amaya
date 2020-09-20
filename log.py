93d46dd8756e06ba5e86f71891b18aca885892d3
codeboy <michal.sk.com@gmail.com>
2020-09-20 19:31:17 +0200

    Changed logging format

--- log.py
+++ log.py
@@ -6,7 +6,7 @@ logger.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('[%(asctime)s] - [%(funcName)s]: %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+formatter = logging.Formatter('[%(asctime)s](%(levelname)s)- [%(funcName)s]: %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
 
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
