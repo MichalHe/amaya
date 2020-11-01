4fdaad556949ff441c82134b8c5a1a75e6e5604f
codeboy <michal.sk.com@gmail.com>
2020-11-01 16:54:29 +0100

    Implemented functions for transforming formulas to processable form.

--- parse.py
+++ parse.py
@@ -2,7 +2,6 @@ from typing import List, Any
 
 
 def lex(source: str) -> List[str]:
-    # Prepare source
     source = source.replace('(', '( ').replace(')', ' )')
     tokens = source.split()
     return tokens
@@ -32,6 +31,49 @@ def filter_asserts(ast):
         if top_level_expr[0] == 'assert':
             _asserts.append(top_level_expr)
     return _asserts
+
+# ["assert" ["not" ["exists" [BINDERS] TERM]]
+
+
+def expand_multivariable_bindings(assertion_tree):
+    if assertion_tree[0] in ['exists', 'forall']:
+        binding_type, bindings, term = assertion_tree
+        assert len(bindings) > 0
+
+        if len(bindings) >= 2:
+            # We are dealing with multivariable exits
+            # ["exists" [x Type, y Type, ...] TERM]]
+            leftmost_binding = bindings[0]
+            tail = bindings[1:]
+
+            # Prepare substatement
+            substmt = [binding_type, tail, term]
+
+            # Update tree
+            assertion_tree[1] = [leftmost_binding]
+            assertion_tree[2] = substmt
+
+    if assertion_tree[0] in ['exists', 'not', 'forall', 'assert']:
+        expand_multivariable_bindings(assertion_tree[-1])
+
+
+def replace_forall_with_exists(assertion_tree):
+    if assertion_tree[0] == 'forall':
+        forall_kw, binders, stmt = assertion_tree
+
+        not_stmt = ['not', stmt]
+        exists = ['exists', binders, not_stmt]
+
+        assertion_tree[0] = 'not'
+        assertion_tree[1] = exists
+        assertion_tree.pop(-1)
+
+    if assertion_tree[0] in ['exists', 'not', 'forall', 'assert']:
+        replace_forall_with_exists(assertion_tree[-1])
+
+
+def remove_multiple_negations(assertion_tree):
+    pass
 
 
 def get_formula(_assert):
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,6 +2,7 @@ import pytest
 import parse
 import re
 import utils
+from typing import List
 
 
 @pytest.fixture
@@ -19,6 +20,47 @@ def smt_formula():
 @pytest.fixture
 def smt_lex_output(smt_formula):
     return parse.lex(smt_formula)
+
+
+@pytest.fixture
+def plain_tree_multivar_exits():
+    return ['assert',
+            ['not',
+             ['exists',
+              [['X', 'TypeX'], ['Y', 'TypeY'], ['Z', 'TypeZ']],
+              ['FORMULA']]]]
+
+
+@pytest.fixture
+def plain_tree_multivar_forall():
+    return ['assert',
+            ['forall',
+             [['X', 'TypeX'], ['Y', 'TypeY']],
+             ['FORMULA']]]
+
+
+@pytest.fixture
+def plain_tree_multivar_mixed():
+    return ['assert',
+            ['forall',
+             [['X', 'TypeX'], ['Y', 'TypeY']],
+             ['exists',
+              [['Z', 'TypeZ'], ['W', 'TypeW']],
+              ['forall',
+               [['Q', 'TypeQ']],
+               ['FORMULA']]
+              ]]]
+
+
+def get_node_name_sequence(root, size: int) -> List[str]:
+    node_names: List[int] = []
+
+    for i in range(size):
+        node_names.append(root[0])
+        root = root[-1]
+        assert type(root) == list
+
+    return node_names
 
 
 def test_lex(smt_formula):
@@ -47,3 +89,68 @@ def test_parse(smt_lex_output):
     assert ['exit'] in expr_tree
 
     assert utils.tree_depth(assert_expr_subtree) > 2
+
+
+def test_expand_multivariable_bindings_simple(plain_tree_multivar_exits,
+                                              plain_tree_multivar_forall):
+    parse.expand_multivariable_bindings(plain_tree_multivar_exits[1])
+    assert(plain_tree_multivar_exits)
+
+    exists_subtree_root = plain_tree_multivar_exits[1][1]
+
+    for i in range(3):
+        assert 'exists' == exists_subtree_root[0]
+        assert len(exists_subtree_root[1]) == 1
+
+        exists_subtree_root = exists_subtree_root[2]
+
+    parse.expand_multivariable_bindings(plain_tree_multivar_forall[1])
+    assert(plain_tree_multivar_forall)
+
+    forall_tree_root = plain_tree_multivar_forall[1]
+    for i in range(2):
+        assert 'forall' == forall_tree_root[0]
+        assert len(forall_tree_root[1]) == 1
+
+
+def test_expand_multivariable_bindings_mixed(plain_tree_multivar_mixed):
+    parse.expand_multivariable_bindings(plain_tree_multivar_mixed[1])
+    assert(plain_tree_multivar_mixed)
+
+    overall_bindings_count = 5
+    bindings_tree_root = plain_tree_multivar_mixed[1]
+    expected_binders_seq = ['forall', 'forall', 'exists', 'exists', 'forall']
+    for i in range(overall_bindings_count):
+        assert expected_binders_seq[i] == bindings_tree_root[0]
+        assert len(bindings_tree_root[1]) == 1
+
+        bindings_tree_root = bindings_tree_root[2]
+
+    assert 'FORMULA' == bindings_tree_root[0]
+
+
+def test_replace_forall_with_exists_simple(plain_tree_multivar_forall):
+    parse.replace_forall_with_exists(plain_tree_multivar_forall)
+    assert(plain_tree_multivar_forall)
+
+    node_names = get_node_name_sequence(plain_tree_multivar_forall[1], 3)
+    assert ['not', 'exists', 'not'] == node_names
+
+
+def test_replace_forall_with_exists_mixed(plain_tree_multivar_mixed):
+    parse.replace_forall_with_exists(plain_tree_multivar_mixed)
+    assert plain_tree_multivar_mixed
+    node_names = get_node_name_sequence(plain_tree_multivar_mixed, 8)
+    expected_node_seq = ['assert', 'not', 'exists', 'not', 'exists', 'not', 'exists', 'not']
+
+    assert expected_node_seq == node_names
+
+
+def test_formula_cleanup(plain_tree_multivar_mixed):
+    parse.replace_forall_with_exists(plain_tree_multivar_mixed)
+    parse.expand_multivariable_bindings(plain_tree_multivar_mixed)
+
+    expected = ['not', 'exists', 'exists', 'not', 'exists', 'exists', 'not', 'exists', 'not']
+    actual = get_node_name_sequence(plain_tree_multivar_mixed[1], len(expected))
+
+    assert expected == actual
