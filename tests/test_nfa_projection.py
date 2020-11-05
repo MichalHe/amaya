9bb134654ec22f55461068f0f83a6f90021692e4
codeboy <michal.sk.com@gmail.com>
2020-11-05 20:47:07 +0100

    Removed check for smaller alphabet after projection.

--- tests/test_nfa_projection.py
+++ tests/test_nfa_projection.py
@@ -10,7 +10,8 @@ def test_nfa_projection(nfa1: NFA):  # NOQA
     assert pnfa.final_states == nfa1.final_states
     assert pnfa.initial_states == nfa1.initial_states
 
-    assert len(pnfa.alphabet.symbols) == (len(nfa1.alphabet.symbols)/2)
+    # Size of the alphabet should not be changed.
+    assert len(pnfa.alphabet.symbols) == (len(nfa1.alphabet.symbols))
 
     expected_symbols_projection = {  # x was the first variable
         (0, 0): ('*', 0),
