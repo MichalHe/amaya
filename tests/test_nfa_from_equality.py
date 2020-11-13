5cd307dea04c6fc01da34c75977d6b3b58a80f67
codeboy <michal.sk.com@gmail.com>
2020-11-13 11:25:36 +0100

    Tests for equality now check for the pressence of TRAP state.

--- tests/test_nfa_from_equality.py
+++ tests/test_nfa_from_equality.py
@@ -18,28 +18,40 @@ def test_build_nfa_from_equality(simple_equation: Relation):
     nfa = build_nfa_from_equality(simple_equation)
 
     assert len(nfa.final_states) == 1
-    assert len(nfa.states) == 5
+    assert len(nfa.states) == 6
 
-    expected_states = [2, 1, 0, -1, 'FINAL']
+    expected_states = [2, 1, 0, -1, 'FINAL', 'TRAP']
     for e_state in expected_states:
         assert e_state in nfa.states
 
     expected_transitions = [
         (2, (0, 0), 1),
         (2, (1, 0), 0),
+        (2, (0, 1), 'TRAP'),
+        (2, (1, 1), 'TRAP'),
 
+        (1, (0, 0), 'TRAP'),
         (1, (0, 1), 1),
-        (1, (0, 1), 'FINAL'),
+        (1, (1, 0), 'TRAP'),
         (1, (1, 1), 0),
+        (1, (0, 1), 'FINAL'),
 
         (0, (0, 0), 0),
-        (0, (0, 0), 'FINAL'),
+        (0, (0, 1), 'TRAP'),
         (0, (1, 0), -1),
+        (0, (1, 1), 'TRAP'),
+        (0, (0, 0), 'FINAL'),
 
+        (-1, (0, 0), 'TRAP'),
+        (-1, (0, 1), 0),
+        (-1, (1, 0), 'TRAP'),
         (-1, (1, 1), -1),
         (-1, (1, 1), 'FINAL'),
-        (-1, (0, 1), 0)
 
+        ('TRAP', (0, 0), 'TRAP'),
+        ('TRAP', (0, 1), 'TRAP'),
+        ('TRAP', (1, 0), 'TRAP'),
+        ('TRAP', (1, 1), 'TRAP'),
     ]
 
     actual_transitions = list(iter_transition_fn(nfa.transition_fn))
