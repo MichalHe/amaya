26d29681fdbedd1a8067a771e254030fff474048
codeboy <michal.sk.com@gmail.com>
2020-12-04 18:46:37 +0100

    Removed explicit parse_op to name convertion in debuging.

--- debug_single.py
+++ debug_single.py
@@ -5,17 +5,6 @@ import os
 import argparse
 import functools
 
-OPERATION_NAMES = {
-    parse.ParsingOperation.BUILD_NFA_FROM_INEQ: 'build_from_ineq',
-    parse.ParsingOperation.BUILD_NFA_FROM_SHARP_INEQ: 'build_from_sharp_ineq',
-    parse.ParsingOperation.BUILD_NFA_FROM_EQ: 'build_from_sharp_ineq',
-    parse.ParsingOperation.NFA_UNION: 'union',
-    parse.ParsingOperation.NFA_PROJECTION: 'projection',
-    parse.ParsingOperation.NFA_COMPLEMENT: 'complement',
-    parse.ParsingOperation.NFA_DETERMINIZE: 'determinize',
-    parse.ParsingOperation.NFA_INTERSECT: 'intersect',
-}
-
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('input_file',
                         help='File containing the SMT2 source for automaton under debug.',
@@ -48,7 +37,7 @@ automaton_cnt = 0
 def nfa_emitted_handle(destination, nfa, parse_op):
     global automaton_cnt
     automaton_graphviz = visualization.convert_automaton_to_graphviz(nfa)
-    operation_name = OPERATION_NAMES[parse_op]
+    operation_name = parse_op.value
     output_path = os.path.join(destination, f'debug_{automaton_cnt}_{operation_name}.gv')
     with open(output_path, 'w') as output_file:
         output_file.write(str(automaton_graphviz))
