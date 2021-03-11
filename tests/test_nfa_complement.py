3a15aee3a31c2e8e926e268c4765e7a471937b37
codeboy <michal.sk.com@gmail.com>
2021-03-11 17:13:23 +0100

    Fixed complement tests to reflect the nonempty complement.

--- tests/test_nfa_complement.py
+++ tests/test_nfa_complement.py
@@ -4,12 +4,14 @@ from utils import iter_transition_fn
 
 
 def test_nfa_complement(nfa1: NFA):  # NOQA
-    non_final_states = nfa1.states - nfa1.final_states
+    non_final_states = nfa1.states - nfa1.final_states - nfa1.initial_states
     assert len(non_final_states) < len(nfa1.states)
 
     compl_nfa = nfa1.complement()
     assert compl_nfa
-    assert len(compl_nfa.final_states) > len(nfa1.final_states)
+
+    # The nfa1 has 3 states, one is final, one initial, one ordinary --> after complement there will be exactly 1 final, 1 nonfinal
+    assert len(compl_nfa.final_states) == len(nfa1.final_states)
 
     for fstate in nfa1.final_states:
         assert fstate not in compl_nfa.final_states
@@ -17,4 +19,4 @@ def test_nfa_complement(nfa1: NFA):  # NOQA
     assert non_final_states == compl_nfa.final_states
     assert len(nfa1.initial_states) == len(compl_nfa.initial_states)
 
-    assert len(tuple(iter_transition_fn(nfa1.transition_fn))) == len(tuple(iter_transition_fn(compl_nfa.transition_fn)))
+    assert len(tuple(iter_transition_fn(nfa1.transition_fn))) == len(tuple(iter_transition_fn(compl_nfa.transition_fn)))  # type: ignore
