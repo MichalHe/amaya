b1b2935e6b9006465423cdf3819f0cc391266f35
codeboy <michal.sk.com@gmail.com>
2021-04-18 01:04:02 +0200

    Changed tests implementation to prevent them from using internals.

--- tests/test_nfa_state_rename.py
+++ tests/test_nfa_state_rename.py
@@ -1,5 +1,5 @@
 import pytest
-from automatons import NFA, AutomatonType
+from automatons import NFA, AutomatonType, LSBF_Alphabet, MTBDD_NFA
 from relations_structures import Relation
 from pressburger_algorithms import build_nfa_from_inequality
 from typing import (
@@ -9,17 +9,32 @@ from typing import (
 
 
 @pytest.fixture
-def nfa() -> NFA:
+def ineq() -> Relation:
     ineq = Relation(
         variable_names=['x', 'y'],
         variable_coeficients=[2, -1],
         absolute_part=3,
         operation="<="
     )
-    return build_nfa_from_inequality(ineq)
+    return ineq
 
 
-def test_state_renaming(nfa):
+alphabet = LSBF_Alphabet.from_variable_names(['1', '2'])
+
+
+def test_nfa_state_rename_w_simple_tfn(ineq: Relation):
+    nfa = build_nfa_from_inequality(ineq, alphabet, NFA)
+    assert nfa, 'Build nfa from ineq did not return a NFA'
+    _test_state_renaming(nfa)
+
+
+def test_nfa_state_rename_w_mtbdd_tfn(ineq: Relation):
+    nfa = build_nfa_from_inequality(ineq, alphabet, MTBDD_NFA)
+    assert nfa, 'Build nfa from ineq did not return a NFA'
+    _test_state_renaming(nfa)
+
+
+def _test_state_renaming(nfa):
     state_names_translat: Dict[Any, int] = dict()
 
     def state_renamed(automaton_id: int, old_state: Any, new_state: int):
@@ -27,23 +42,33 @@ def test_state_renaming(nfa):
 
     assert nfa.automaton_type == AutomatonType.NFA
 
+    before_rename_state_posts = {}
+    for state in nfa.states:
+        before_rename_state_posts[state] = nfa.get_state_post(state)
+
     nfa._debug_state_rename = state_renamed
-
-    has_str_state = len(tuple(filter(lambda state: type(state) == str, nfa.states))) > 0
-    assert has_str_state
-
     _, new_nfa = nfa.rename_states()
+
+    after_rename_state_posts = {}
+    for state in new_nfa.states:
+        after_rename_state_posts[state] = new_nfa.get_state_post(state)
 
     assert new_nfa
     assert nfa.automaton_type == AutomatonType.NFA
     assert len(state_names_translat) == len(new_nfa.states)
 
-    for original_origin in nfa.states:
-        new_origin = state_names_translat[original_origin]
-        if original_origin not in nfa.transition_fn.data:
-            continue
+    assert len(before_rename_state_posts) == len(after_rename_state_posts),\
+        'The dicts containing post sets before and after renaming should be the same size'
 
-        for original_dest in nfa.transition_fn.data[original_origin]:
-            new_dest = state_names_translat[original_dest]
-            assert new_origin in new_nfa.transition_fn.data
-            assert new_dest in new_nfa.transition_fn.data[new_origin]
+    for old_state in before_rename_state_posts:
+        new_state = state_names_translat[old_state]
+
+        before_posts = before_rename_state_posts[old_state]
+        after_posts = after_rename_state_posts[new_state]
+
+        assert len(before_posts) == len(after_posts),\
+            f'The state posts of {old_state}->{new_state} should have the same size after rename'
+
+        # Try rename the old post to the new, and see if they match
+        old_post_renames = set(map(lambda old_name: state_names_translat[old_name], before_posts))
+        assert old_post_renames == set(after_posts)
--- tests/test_nfa_union.py
+++ tests/test_nfa_union.py
@@ -1,40 +1,49 @@
 import pytest
-from automatons import NFA
+from automatons import NFA, LSBF_Alphabet, MTBDD_NFA
 from relations_structures import Relation
 from pressburger_algorithms import build_nfa_from_inequality
 from utils import transition_fn_size
 from transitions import iter_transition_fn
 from log import logger
+from collections import namedtuple
 from typing import (
     Dict,
     Any
 )
 
+alphabet = LSBF_Alphabet.from_variable_names([1, 2])
+
 
 @pytest.fixture
-def nfa1() -> NFA:
+def ineq0() -> Relation:
     ineq = Relation(
         variable_names=['x', 'y'],
         variable_coeficients=[2, -1],
         absolute_part=2,
         operation='<='
     )
-    return build_nfa_from_inequality(ineq)
+    return ineq
 
 
 @pytest.fixture
-def nfa2() -> NFA:
+def ineq1() -> Relation:
     ineq = Relation(
         variable_names=['x', 'y'],
         variable_coeficients=[3, -1],
         absolute_part=3, operation='<='
     )
-    return build_nfa_from_inequality(ineq)
+    return ineq
 
 
-def test_automaton_union(nfa1, nfa2):
-    nfa_1_state_count = len(nfa1.states)
-    nfa_2_state_count = len(nfa2.states)
+def do_test_automaton_union(nfa1, nfa2):
+    StateSizes = namedtuple('AutomatonSizeStats',
+                            ['states', 'initial_states', 'final_states'])
+
+    nfa1_sizes = StateSizes(len(nfa1.states), len(nfa1.initial_states), len(nfa1.final_states))
+    nfa2_sizes = StateSizes(len(nfa2.states), len(nfa2.initial_states), len(nfa2.final_states))
+
+    nfa1_transitions = list(nfa1.transition_fn.iter())
+    nfa2_transitions = list(nfa2.transition_fn.iter())
 
     nfa1_state_translation: Dict[Any, int] = dict()
     nfa2_state_translation: Dict[Any, int] = dict()
@@ -54,17 +63,12 @@ def test_automaton_union(nfa1, nfa2):
     nfa2._debug_state_rename = nfa_state_renamed
 
     united_automaton = nfa1.union(nfa2)
-    assert len(united_automaton.states) == nfa_1_state_count + nfa_2_state_count
-    assert len(united_automaton.initial_states) == len(nfa1.initial_states) + len(nfa2.initial_states)
-    assert len(united_automaton.final_states) == len(nfa1.final_states) + len(nfa2.final_states)
+    assert len(united_automaton.states) == nfa1_sizes.states + nfa2_sizes.states
+    assert len(united_automaton.initial_states) == nfa1_sizes.initial_states + nfa2_sizes.initial_states
+    assert len(united_automaton.final_states) == nfa1_sizes.final_states + nfa2_sizes.final_states
 
-    for final_state in united_automaton.final_states:
-        assert final_state in united_automaton.final_states
+    assert len(list(united_automaton.transition_fn.iter())) == len(nfa1_transitions) + len(nfa2_transitions)
 
-    for initial_state in united_automaton.initial_states:
-        assert initial_state in united_automaton.initial_states
-
-    assert transition_fn_size(united_automaton.transition_fn.data) == transition_fn_size(nfa1.transition_fn.data) + transition_fn_size(nfa2.transition_fn.data)
     assert len(nfa1_state_translation) > 0
     assert len(nfa2_state_translation) > 0
 
@@ -81,3 +85,15 @@ def test_automaton_union(nfa1, nfa2):
         translated_dest = nfa2_state_translation[d]
 
         assert s in united_automaton.transition_fn.data[translated_origin][translated_dest]
+
+
+def test_sparse_nfa_union(ineq0, ineq1):
+    nfa0 = build_nfa_from_inequality(ineq0, alphabet, NFA)
+    nfa1 = build_nfa_from_inequality(ineq1, alphabet, NFA)
+    do_test_automaton_union(nfa0, nfa1)
+
+
+def test_mtbdd_nfa_union(ineq0, ineq1):
+    mtbdd_nfa0 = build_nfa_from_inequality(ineq0, alphabet, MTBDD_NFA)
+    mtbdd_nfa1 = build_nfa_from_inequality(ineq1, alphabet, MTBDD_NFA)
+    do_test_automaton_union(mtbdd_nfa0, mtbdd_nfa1)
