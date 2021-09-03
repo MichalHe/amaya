fd76a33f8175595558a1458ef2f62b280da0d203
codeboy <michal.sk.com@gmail.com>
2021-09-03 22:51:37 +0200

    Relation with mod terms -> DFA tests: add test for an equation with 1 mt

--- tests/test_build_dfa_from_modulo_atomic_constraint.py
+++ tests/test_build_dfa_from_modulo_atomic_constraint.py
@@ -33,18 +33,20 @@ def ineq_with_single_mod_term() -> RelationSetup:
     return (variable_id_pairs, alphabet, relation)
 
 
-def test_inequality_with_single_modulo_constaint(ineq_with_single_mod_term: RelationSetup):
-    '''
-    Tests whether the automaton built from an inequality with single modulo term works as expected.
-    '''
+@pytest.fixture
+def eq_with_single_mod_term(ineq_with_single_mod_term: RelationSetup) -> RelationSetup:
+    '''Equation (x mod 3) = 1'''
 
-    variable_id_pairs, alphabet, inequation = ineq_with_single_mod_term
-    nfa = NFA(alphabet=alphabet, automaton_type=AutomatonType.DFA)
+    inequation = ineq_with_single_mod_term[2]
+    inequation.operation = '='
+    return ineq_with_single_mod_term
 
-    build_presburger_modulo_dfa(inequation, variable_id_pairs, alphabet, nfa)
 
+def assert_single_mod_term_automaton_structure(nfa: NFA,
+                                               final_state_count: int) -> Tuple[ResolutionState, ResolutionState, ResolutionState]:
+    '''Verifies that the automaton for relation (x mod 3) <OP> 1 has the expected structure.'''
     assert len(nfa.initial_states), 'The dfa build should have exactly 1 initial state.'
-    assert len(nfa.final_states) == 2, 'The dfa should have 2 final states - 0 and 1'
+    assert len(nfa.final_states) == final_state_count, 'The dfa should have 2 final states - 0 and 1'
     assert len(nfa.states) == 3, 'The dfa should have a state for every possible remainder - that is 3.'
 
     # States for reminders (rem=reminder)
@@ -70,3 +72,32 @@ def test_inequality_with_single_modulo_constaint(ineq_with_single_mod_term: Rela
         assert len(destination) == 1, 'The states should have only 1 transition for every symbol.'
 
         expected_destination.bind(list(destination)[0])
+
+    return (rem0, rem1, rem2)
+
+
+def test_inequality_with_single_modulo_constaint(ineq_with_single_mod_term: RelationSetup):
+    '''
+    Tests whether the automaton built from an inequality with single modulo term works as expected.
+    '''
+
+    variable_id_pairs, alphabet, inequation = ineq_with_single_mod_term
+    nfa = NFA(alphabet=alphabet, automaton_type=AutomatonType.DFA)
+
+    build_presburger_modulo_dfa(inequation, variable_id_pairs, alphabet, nfa)
+
+    rem0, rem1, rem2 = assert_single_mod_term_automaton_structure(nfa, 2)
+
+    assert rem0.get() in nfa.final_states
+    assert rem1.get() in nfa.final_states
+
+
+def test_equality_with_single_modulo_constaint(eq_with_single_mod_term: RelationSetup):
+    variable_id_pairs, alphabet, equation = eq_with_single_mod_term
+    nfa = NFA(alphabet=alphabet, automaton_type=AutomatonType.DFA)
+
+    build_presburger_modulo_dfa(equation, variable_id_pairs, alphabet, nfa)
+
+    rem0, rem1, rem2 = assert_single_mod_term_automaton_structure(nfa, 1)
+
+    assert rem1.get() in nfa.final_states
