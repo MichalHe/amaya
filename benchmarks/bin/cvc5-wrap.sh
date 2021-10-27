829cf7f6a1ce8fe94f5717ca502e90f370fd1ede
codeboy <michal.sk.com@gmail.com>
2021-10-27 14:05:36 +0200

    Make benchmarks execute cvc5 instead of cvc4.

---
+++ benchmarks/bin/cvc5-wrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check the number of command-line arguments
+if [ \( "$#" -ne 1 \) ] ; then
+	echo "usage: ${0} <input>"
+	exit 1
+fi
+
+INPUT=$1
+
+out=$(cvc5 --lang smt2 ${INPUT})
+ret=$?
+echo "result: ${out}"
+
+exit ${ret}
