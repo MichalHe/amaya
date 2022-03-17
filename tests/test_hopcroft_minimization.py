9d8ae256039b3045d8e592d83fc0551b93edcb82
codeboy <michal.sk.com@gmail.com>
2022-03-17 09:21:27 +0100

    hopcroft minimization: extend unit tests

--- tests/test_hopcroft_minimization.py
+++ tests/test_hopcroft_minimization.py
@@ -1,6 +1,7 @@
 from alphabet import LSBF_Alphabet
 from automatons import (
     AutomatonType,
+    DFA,
     NFA
 )
 from tests.conftest import ResolutionState
@@ -52,6 +53,37 @@ def wiki_automaton() -> NFA:
     return nfa
 
 
+@pytest.fixture
+def automaton2() -> DFA:
+    variable_id_pairs = [('x', 1)]
+    alphabet = LSBF_Alphabet.from_variable_id_pairs(variable_id_pairs)
+
+    dfa = DFA(alphabet=alphabet,
+              automaton_type=AutomatonType.DFA,
+              states={0, 1, 2, 3, 4, 5},
+              final_states={3, 5},
+              initial_states={0})
+
+    transitions = (
+        (0, (0,), 1),
+        (0, (1,), 3),
+        (1, (0,), 0),
+        (1, (1,), 3),
+        (2, (0,), 1),
+        (2, (1,), 4),
+        (3, (0,), 5),
+        (3, (1,), 5),
+        (4, (0,), 3),
+        (4, (1,), 3),
+        (5, (0,), 5),
+        (5, (1,), 5),
+    )
+
+    for transition in transitions:
+        dfa.update_transition_fn(*transition)
+    return dfa
+
+
 def test_wiki_automaton(wiki_automaton: NFA):
     """
     Checks that the hopcrofts minimization works correctly on the automaton found on Wikipedia minimiazation article.
@@ -59,7 +91,7 @@ def test_wiki_automaton(wiki_automaton: NFA):
     Link: https://en.wikipedia.org/wiki/DFA_minimization#/media/File:DFA_to_be_minimized.jpg
     Link (minimized): https://en.wikipedia.org/wiki/DFA_minimization#/media/File:Minimized_DFA.jpg
     """
-    
+
     state0 = ResolutionState('0')
     state1 = ResolutionState('1')
     state2 = ResolutionState('2')
@@ -70,7 +102,7 @@ def test_wiki_automaton(wiki_automaton: NFA):
 
         (state1, (0,), state1),
         (state1, (1,), state2),
-        
+
         (state2, (0,), state2),
         (state2, (1,), state2),
     )
@@ -92,3 +124,35 @@ def test_wiki_automaton(wiki_automaton: NFA):
         dest_rs.bind(dest)
 
     assert state1.get() in minimized_automaton.final_states
+
+
+def test_automaton2(automaton2: NFA):
+
+    state0 = ResolutionState('0')
+    state1 = ResolutionState('1')
+    state2 = ResolutionState('2')
+
+    expected_transitions = (
+        (state0, (0,), state1),
+        (state0, (1,), state2),
+        (state1, (0,), state0),
+        (state1, (1,), state2),
+        (state2, (0,), state2),
+        (state2, (1,), state2),
+    )
+
+    minimized_dfa = automaton2.hopcroft_minimization()
+
+    assert len(minimized_dfa.states) == 3
+    assert len(minimized_dfa.initial_states) == 1
+    assert len(minimized_dfa.final_states) == 1
+
+    state0.bind(next(iter(minimized_dfa.initial_states)))
+
+    # _rs stands for resolution state
+    for source_rs, symbol, dest_rs in expected_transitions:
+        post = minimized_dfa.get_transition_target(source_rs.get(), symbol)
+        assert len(post) == 1, 'Minimized automaton is not DFA'
+        dest_rs.bind(next(iter(post)))
+
+    assert state2.get() in minimized_dfa.final_states
