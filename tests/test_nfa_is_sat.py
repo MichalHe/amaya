b8ed2dc2a1f48f9fb4df82303a73157f481be910
codeboy <michal.sk.com@gmail.com>
2022-04-06 21:26:47 +0200

    Extend is_sat tests to also cover MTBDD automata

--- tests/test_nfa_is_sat.py
+++ tests/test_nfa_is_sat.py
@@ -5,10 +5,16 @@ from amaya.automatons import (
     AutomatonType,
     NFA, 
 )
+from amaya.mtbdd_automatons import (
+    MTBDD_NFA, 
+)
 
+from amaya.presburger.definitions import (
+    AutomatonConstructor
+)
 
-def nfa_containing_model() -> NFA:
-    nfa = NFA(
+def nfa_containing_model(constr: AutomatonConstructor) -> NFA:
+    nfa = constr(
         alphabet=LSBF_Alphabet.from_variable_id_pairs([('x', 1), ('y', 2)]),
         automaton_type=AutomatonType.NFA,
         states={0, 1, 2, 3, 4},
@@ -34,8 +40,8 @@ def nfa_containing_model() -> NFA:
     return nfa
 
 
-def nfa_without_model() -> NFA:
-    nfa = NFA(
+def nfa_without_model(constr: AutomatonConstructor) -> NFA:
+    nfa = constr(
         alphabet=LSBF_Alphabet.from_variable_id_pairs([('x', 1), ('y', 2)]),
         automaton_type=AutomatonType.NFA,
         states={0, 1, 2, 3, 4},
@@ -65,8 +71,10 @@ def nfa_without_model() -> NFA:
 @pytest.mark.parametrize(
     ('nfa', 'expected_model'), 
     (
-        (nfa_containing_model(), ((1, 1), (1, 1), (1, 1))),
-        (nfa_without_model(), None),
+        (nfa_containing_model(NFA), ((1, 1), (1, 1), (1, 1))),
+        (nfa_containing_model(MTBDD_NFA), ((1, 1), (1, 1), (1, 1))),
+        (nfa_without_model(NFA), None),
+        (nfa_without_model(MTBDD_NFA), None),
     ))
 def test_find_model(nfa: NFA, expected_model):
     model = nfa.find_model()
