dcaefaa388ec160aefc67e622a36db1d4f4ea2cb
codeboy <michal.sk.com@gmail.com>
2022-05-10 19:53:30 +0200

    Add tests for boolean automata

--- tests/test_boolean_automatons.py
+++ tests/test_boolean_automatons.py
@@ -1,103 +1,42 @@
+from amaya.alphabet import LSBF_Alphabet
+from amaya.automatons import NFA
+from amaya.config import BackendType
+from amaya.mtbdd_automatons import MTBDD_NFA
+
 import pytest
-from parse import (
-    run_evaluation_procedure,
-    EvaluationContext,
-    EvaluationConfig,
-    BackendType,
-    SolutionDomain,
-    VariableType,
-)
-from automatons import (
-    NFA,
-    LSBF_Alphabet
-)
 
 
-@pytest.fixture
-def conj_bool_relation_term():
-    term = [
-        'and',
-        'b0',
-        ['<=', ['*', '2', 'x'], 1]
-    ]
-
-    return term
+alphabet = LSBF_Alphabet.from_variable_id_pairs([('x', 1)])
 
 
-@pytest.mark.parametrize(('backend_type',), [(BackendType.NAIVE,), (BackendType.MTBDD,)])
-def test_conj_bool_with_relation(conj_bool_relation_term, backend_type):
-    eval_cfg = EvaluationConfig(
-        solution_domain=SolutionDomain.INTEGERS,
-        backend_type=backend_type
-    )
+@pytest.mark.parametrize(('automaton_cls',), [(NFA,), (MTBDD_NFA,)])
+def test_x_and_not_x(automaton_cls: NFA):
+    x_nfa = automaton_cls.for_bool_variable(overall_alphabet=alphabet, var_id=1, var_value=True)
+    not_x_nfa = automaton_cls.for_bool_variable(overall_alphabet=alphabet, var_id=1, var_value=True).complement()
+    
+    result = x_nfa.intersection(not_x_nfa)
 
-    alphabet = LSBF_Alphabet.from_variable_ids([1, 2])
-    ctx = EvaluationContext(eval_cfg, alphabet=alphabet)
-    ctx.add_global_variable('b0', VariableType.BOOL)
-    ctx.add_global_variable('x', VariableType.INT)
+    assert len(result.final_states) == 0  # The language of the resulting automaton should be empty
 
-    nfa = run_evaluation_procedure(
-        conj_bool_relation_term,
-        ctx)
 
-    assert nfa
-    assert len(nfa.initial_states) == 1
+@pytest.mark.parametrize(('automaton_cls',), [(NFA,), (MTBDD_NFA,)])
+def test_x_or_not_x(automaton_cls: NFA):
+    x_nfa = automaton_cls.for_bool_variable(overall_alphabet=alphabet, var_id=1, var_value=True)
+    not_x_nfa = automaton_cls.for_bool_variable(overall_alphabet=alphabet, var_id=1, var_value=True).determinize().complement()
+
+    result: NFA = x_nfa.union(not_x_nfa).determinize().complement()
+    assert not result.final_states
+
+
+@pytest.mark.parametrize(('automaton_cls',), [(NFA,), (MTBDD_NFA,)])
+def test_not_x(automaton_cls: NFA):
+    nfa: NFA = automaton_cls.for_bool_variable(alphabet, var_id=1, var_value=False)
+
+    assert len(nfa.states) == 2  # States (except intiial) have universal/empty language - the empty might be removed
     assert len(nfa.final_states) == 1
-    assert len(nfa.states) == 5
-
-    init_state = list(nfa.initial_states)[0]
-    final_state = list(nfa.final_states)[0]
-
-    assert len(nfa.get_transition_target(init_state, (1, 0))) == 2
-    assert final_state in nfa.get_transition_target(init_state, (1, 0))
-
-    assert len(nfa.get_transition_target(init_state, (1, 1))) == 2
-    assert final_state in nfa.get_transition_target(init_state, (1, 1))
-
-    q1 = list(set(nfa.get_transition_target(init_state, (1, 1))) - nfa.final_states)[0]
-
-    assert len(nfa.get_transition_target(q1, (1, 1))) == 2
-    assert len(nfa.get_transition_target(q1, (0, 1))) == 2
-    assert final_state in nfa.get_transition_target(q1, (1, 1))
-    assert final_state in nfa.get_transition_target(q1, (0, 1))
-
-    q2 = list(set(nfa.get_transition_target(q1, (1, 1))) - nfa.final_states)[0]
-    assert len(nfa.get_transition_target(q2, (1, 1))) == 2
-
-    assert q2 in nfa.get_transition_target(q2, (1, 1))
-    assert final_state in nfa.get_transition_target(q2, (1, 1))
-
-
-def test_bool_automaton_complement():
-    alphabet = LSBF_Alphabet.from_variable_ids([1])
-    nfa_0 = NFA.for_bool_variable(alphabet, var_id=1, var_value=True)
-    nfa = nfa_0.determinize().complement()
-
     assert len(nfa.initial_states) == 1
-    assert len(nfa.final_states) == 1
-    assert len(nfa.states) == 3
 
-    # The bool automaton should have three states after determinization:
-    # initial, final and TRAP
-    init_state = list(nfa.initial_states)[0]
-    final_state = list(nfa.final_states)[0]  # TRAP is final, since it is a complement automaton
-
-    assert final_state in nfa.get_transition_target(init_state, (0, ))
-    assert len(nfa.get_transition_target(init_state, (0, ))) == 1
-
-    assert final_state not in nfa.get_transition_target(init_state, (1, ))
-    assert len(nfa.get_transition_target(init_state, (1, ))) == 1
-
-    assert len(nfa.get_transition_target(final_state, ('*', ))) == 1
-
-    nonfinal_state_set = nfa.states - nfa.initial_states - nfa.final_states
-    nonfinal_state = list(nonfinal_state_set)[0]
-    assert nonfinal_state
-
-    print(nfa.get_visualization_representation())
-
-    assert len(set(nfa.get_transition_target(final_state, ('*', )))) == 1
-
-    # The previous accepting state should have one selfloop the true/false
-    # symbol and one leading to the trap state
-    assert len(set(nfa.get_transition_target(nonfinal_state, ('*', )))) == 2
+    init_state = next(iter(nfa.initial_states))
+    final_state = next(iter(nfa.final_states))
+    dest = nfa.get_transition_target(init_state, (0, )) 
+    assert final_state in dest
