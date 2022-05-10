99ed71d6f0a62944c8def273a4b9c5aa86e9a2c3
codeboy <michal.sk.com@gmail.com>
2022-05-10 20:41:50 +0200

    Fix EqToNFA tests

--- tests/test_nfa_from_equality.py
+++ tests/test_nfa_from_equality.py
@@ -1,53 +1,60 @@
-from relations_structures import Relation
+from amaya.alphabet import LSBF_Alphabet
+from amaya.automatons import NFA
+from amaya.mtbdd_automatons import MTBDD_NFA
+from amaya.presburger.constructions.integers import build_nfa_from_linear_equality
+from amaya.relations_structures import Relation
+from tests.conftest import ResolutionState
+
 import pytest
-from presburger_algorithms import build_nfa_from_linear_equality
-from transitions import iter_transition_fn
-from alphabet import LSBF_Alphabet
-from automatons import NFA
 
 
 @pytest.fixture
 def simple_equation() -> Relation:
-    return Relation(
-        variable_names=['x', 'y'],
-        variable_coeficients=[2, -1],
-        modulo_terms=[],
-        modulo_term_coeficients=[],
-        absolute_part=2,
-        operation='='
-    )
+    return Relation.new_lin_relation(variable_names=['x', 'y'], variable_coeficients=[2, -1],
+                                     absolute_part=2, operation='=')
 
 
-def test_build_nfa_from_equality(simple_equation: Relation):
-    alphabet = LSBF_Alphabet.from_variable_ids([1, 2])
-    nfa = build_nfa_from_linear_equality(simple_equation, [1, 2], alphabet, NFA)
+@pytest.mark.parametrize('automaton_cls', (NFA, MTBDD_NFA))
+def test_build_nfa_from_equality(automaton_cls: NFA, simple_equation: Relation):
+    alphabet = LSBF_Alphabet.from_variable_id_pairs([('x', 1), ('y', 2)])
+    nfa = build_nfa_from_linear_equality(simple_equation, [('x', 1), ('y', 2)], alphabet, automaton_cls)
 
-    expected_states = [2, 1, 0, -1, 'FINAL']
-    for e_state in expected_states:
-        assert e_state in nfa.states
+    expected_states = {str(label): ResolutionState(str(label)) for label in (2, 1, 0, -1, 'qf')}
 
+    assert len(nfa.initial_states) == 1
     assert len(nfa.final_states) == 1
     assert len(nfa.states) == len(expected_states)
 
+    expected_states['2'].bind(next(iter(nfa.initial_states)))
+    expected_states['qf'].bind(next(iter(nfa.final_states)))
+
     expected_transitions = [
-        (2, (0, 0), 1),
-        (2, (1, 0), 0),
+        (expected_states['2'], (0, 0), expected_states['1']),
+        (expected_states['2'], (1, 0), expected_states['0']),
 
-        (1, (0, 1), 1),
-        (1, (1, 1), 0),
-        (1, (0, 1), 'FINAL'),
+        (expected_states['1'], (0, 1), expected_states['1']),
+        (expected_states['1'], (1, 1), expected_states['0']),
 
-        (0, (0, 0), 0),
-        (0, (1, 0), -1),
-        (0, (0, 0), 'FINAL'),
+        (expected_states['0'], (0, 0), expected_states['0']),
+        (expected_states['0'], (1, 0), expected_states['-1']),
 
-        (-1, (0, 1), 0),
-        (-1, (1, 1), -1),
-        (-1, (1, 1), 'FINAL'),
+        (expected_states['-1'], (0, 1), expected_states['0']),
+        (expected_states['-1'], (1, 1), expected_states['-1']),
     ]
 
-    actual_transitions = list(iter_transition_fn(nfa.transition_fn.data))
+    fin_transitions = [
+        (expected_states['1'], (0, 1)),
+        (expected_states['0'], (0, 0)),
+        (expected_states['-1'], (1, 1)),
+    ]
 
-    assert len(actual_transitions) == len(expected_transitions)
-    for actual_t in actual_transitions:
-        assert actual_t in expected_transitions
+    for origin, symbol, dest in expected_transitions:
+        dest_set = set(nfa.get_transition_target(origin.get(), symbol)).difference(nfa.final_states)
+        assert len(dest_set) == 1
+        dest.bind(next(iter(dest_set)))
+
+
+    for origin, symbol in fin_transitions:
+        dest_set = nfa.get_transition_target(origin.get(), symbol)
+        assert len(dest_set) == 2
+        assert expected_states['qf'].get() in dest_set
