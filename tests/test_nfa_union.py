246ab96a5cdfabd0dae95348fbbf3ddf04a6d2a9
codeboy <michal.sk.com@gmail.com>
2022-05-10 20:04:12 +0200

    Fix union tests.

--- tests/test_nfa_union.py
+++ tests/test_nfa_union.py
@@ -1,103 +1,89 @@
-import pytest
-from automatons import NFA
-from alphabet import LSBF_Alphabet
-from mtbdd_automatons import MTBDD_NFA
-from relations_structures import Relation
-from presburger_algorithms import build_nfa_from_linear_inequality
-from log import logger
 from collections import namedtuple
 from typing import (
     Dict,
     Any
 )
 
-alphabet = LSBF_Alphabet.from_variable_ids([1, 2])
+from amaya.alphabet import LSBF_Alphabet
+from amaya.automatons import NFA
+from amaya.mtbdd_automatons import MTBDD_NFA
+from amaya.presburger.constructions.integers import build_nfa_from_linear_inequality
+from amaya.relations_structures import Relation
+
+import pytest
+
+alphabet = LSBF_Alphabet.from_variable_id_pairs([('x', 1), ('y', 2)])
 
 
 @pytest.fixture
 def ineq0() -> Relation:
     return Relation(
-        variable_names=['x', 'y'],
-        variable_coeficients=[2, -1],
-        absolute_part=2,
-        modulo_terms=[],
-        modulo_term_coeficients=[],
-        operation='<='
+        variable_names=['x', 'y'], variable_coeficients=[2, -1],
+        modulo_terms=[], modulo_term_coeficients=[],
+        div_terms=[], div_term_coeficients=[],
+        operation='<=', absolute_part=2,
     )
 
 
 @pytest.fixture
 def ineq1() -> Relation:
     return Relation(
-        variable_names=['x', 'y'],
-        variable_coeficients=[3, -1],
-        absolute_part=3, 
-        operation='<=',
-        modulo_terms=[],
-        modulo_term_coeficients=[]
+        variable_names=['x', 'y'], variable_coeficients=[3, -1],
+        modulo_terms=[], modulo_term_coeficients=[],
+        div_terms=[], div_term_coeficients=[],
+        operation='<=', absolute_part=3,
     )
 
 
-def do_test_automaton_union(nfa1, nfa2):
-    StateSizes = namedtuple('AutomatonSizeStats',
-                            ['states', 'initial_states', 'final_states'])
 
+@pytest.mark.parametrize('automaton_cls', (NFA, MTBDD_NFA))
+def test_automaton_union(automaton_cls: NFA, ineq0: Relation, ineq1: Relation):
+    StateSizes = namedtuple('AutomatonSizeStats', ['states', 'initial_states', 'final_states'])
+
+    nfa0 = build_nfa_from_linear_inequality(ineq0, [('x', 1), ('y', 2)], alphabet, automaton_cls)
+    nfa1 = build_nfa_from_linear_inequality(ineq1, [('x', 1), ('y', 2)], alphabet, automaton_cls)
+
+    nfa0_sizes = StateSizes(len(nfa0.states), len(nfa0.initial_states), len(nfa0.final_states))
     nfa1_sizes = StateSizes(len(nfa1.states), len(nfa1.initial_states), len(nfa1.final_states))
-    nfa2_sizes = StateSizes(len(nfa2.states), len(nfa2.initial_states), len(nfa2.final_states))
 
+    nfa0_transitions = list(nfa0.transition_fn.iter())
     nfa1_transitions = list(nfa1.transition_fn.iter())
-    nfa2_transitions = list(nfa2.transition_fn.iter())
 
+    nfa0_state_translation: Dict[Any, int] = dict()
     nfa1_state_translation: Dict[Any, int] = dict()
-    nfa2_state_translation: Dict[Any, int] = dict()
+    nfa0_id = id(nfa0)
     nfa1_id = id(nfa1)
-    nfa2_id = id(nfa2)
 
     def nfa_state_renamed(_id: int, old_state: Any, new_state: int):
-        if _id == nfa1_id:
+        if _id == nfa0_id:
+            nfa0_state_translation[old_state] = new_state
+        elif _id == nfa1_id:
             nfa1_state_translation[old_state] = new_state
-        elif _id == nfa2_id:
-            nfa2_state_translation[old_state] = new_state
         else:
             logger.fatal('Error while renaming automaton states - generated translation for unknown automaton id')
             assert False
 
+    nfa0._debug_state_rename = nfa_state_renamed
     nfa1._debug_state_rename = nfa_state_renamed
-    nfa2._debug_state_rename = nfa_state_renamed
 
-    united_automaton = nfa1.union(nfa2)
-    assert len(united_automaton.states) == nfa1_sizes.states + nfa2_sizes.states
-    assert len(united_automaton.initial_states) == nfa1_sizes.initial_states + nfa2_sizes.initial_states
-    assert len(united_automaton.final_states) == nfa1_sizes.final_states + nfa2_sizes.final_states
+    united_automaton = nfa0.union(nfa1)
+    assert len(united_automaton.states) == nfa0_sizes.states + nfa1_sizes.states
+    assert len(united_automaton.initial_states) == nfa0_sizes.initial_states + nfa1_sizes.initial_states
+    assert len(united_automaton.final_states) == nfa0_sizes.final_states + nfa1_sizes.final_states
 
     union_transitions = list(united_automaton.transition_fn.iter())
-    assert len(union_transitions) == len(nfa1_transitions) + len(nfa2_transitions)
-
-    assert len(nfa1_state_translation) > 0
-    assert len(nfa2_state_translation) > 0
+    assert len(union_transitions) == len(nfa0_transitions) + len(nfa1_transitions)
 
     # Verify that both transition fns are indeed present in the united
     # automaton
+    for o, s, d in nfa0_transitions:
+        to = nfa0_state_translation[o]
+        td = nfa0_state_translation[d]
+
+        assert (to, s, td) in union_transitions
+
     for o, s, d in nfa1_transitions:
         to = nfa1_state_translation[o]
         td = nfa1_state_translation[d]
 
         assert (to, s, td) in union_transitions
-
-    for o, s, d in nfa2_transitions:
-        to = nfa2_state_translation[o]
-        td = nfa2_state_translation[d]
-
-        assert (to, s, td) in union_transitions
-
-
-def test_sparse_nfa_union(ineq0, ineq1):
-    nfa0 = build_nfa_from_linear_inequality(ineq0, [('x', 1), ('y', 2)], alphabet, NFA)
-    nfa1 = build_nfa_from_linear_inequality(ineq1, [('x', 1), ('y', 2)], alphabet, NFA)
-    do_test_automaton_union(nfa0, nfa1)
-
-
-def test_mtbdd_nfa_union(ineq0, ineq1):
-    mtbdd_nfa0 = build_nfa_from_linear_inequality(ineq0, [('x', 1), ('y', 2)], alphabet, MTBDD_NFA)
-    mtbdd_nfa1 = build_nfa_from_linear_inequality(ineq1, [('x', 1), ('y', 2)], alphabet, MTBDD_NFA)
-    do_test_automaton_union(mtbdd_nfa0, mtbdd_nfa1)
