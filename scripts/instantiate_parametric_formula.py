f713153fc2a8564fa585aa35f3a0b3aeebca2d35
mhecko <michal.sk.com@gmail.com>
2023-04-04 12:03:16 +0200

    scripts(instantiate_formula): take a file with list of param values

--- scripts/instantiate_parametric_formula.py
+++ scripts/instantiate_parametric_formula.py
@@ -1,24 +1,45 @@
 import argparse
 import sys
+import os
 
 
-class KeyValAction(argparse.Action):
-    def __call__(self, parser, namespace, value, option_string=None):
-        fragments = value.split(':')
-        if len(fragments) != 2:
-            raise ValueError(f'The value {value} does not have the expected form <key>:<value>')
-        getattr(namespace, self.dest).append(tuple(fragments))
+def load_parameter_values(param_values_path):
+    with open(param_values_path) as values_file:
+        for line in values_file.read().split('\n'):
+            line = line.strip()
+            if line:
+                yield int(line)
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-s', dest='subsitutions', type=str, help='key:value to expand', action=KeyValAction, default=[])
-args = parser.parse_args()
+def instantiate_formula(template_text: str, parameter_name: str, parameter_value: int):
+    key = f'${{{parameter_name}}}'
+    template_text = template_text.replace(key, str(parameter_value))
+    return template_text
 
 
-formula_text = sys.stdin.read()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('formula_template_file')
+    parser.add_argument('parameter_name', help='Name of the parameter to substitute')
+    parser.add_argument('parameter_values')
+    parser.add_argument('output_folder')
+    args = parser.parse_args()
 
-for key, value in args.subsitutions:
-    key = f'${{{key}}}'
-    formula_text = formula_text.replace(key, value)
+    with open(args.formula_template_file) as template_file:
+        template_text = template_file.read()
 
-print(formula_text)
+    if not os.path.isdir(args.output_folder):
+        print('The output folder does not exists!')
+        sys.exit(1)
+
+    template_filename = os.path.basename(args.formula_template_file)
+    filename, ext = template_filename.rsplit('.', maxsplit=1)
+    instance_filename_template = f'{filename}_{{0}}.{ext}'
+
+    for param_value in load_parameter_values(args.parameter_values):
+        formula = instantiate_formula(template_text, args.parameter_name, param_value)
+        output_filename = instance_filename_template.format(f'{args.parameter_name}({param_value})')
+        output_path = os.path.join(args.output_folder, output_filename)
+
+        with open(output_path,  'w') as output_file:
+            output_file.write(formula)
